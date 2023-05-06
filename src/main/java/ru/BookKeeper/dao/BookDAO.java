c1e963c7abd34b1a2cee15d3e7bdef8127645b79
Phaser2028 <dima.2000600@gmail.com>
2023-05-06 14:47:55 +0300

    Empty DAO for more complex queries

---
+++ src/main/java/ru/BookKeeper/dao/BookDAO.java
@@ -0,0 +1,18 @@
+package ru.BookKeeper.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.BookKeeper.models.Book;
+import ru.BookKeeper.models.Person;
+
+
+import java.util.List;
+import java.util.Optional;
+
+
+
+public class BookDAO {
+
+}
---
+++ src/main/java/ru/BookKeeper/dao/PersonDAO.java
@@ -0,0 +1,19 @@
+package ru.BookKeeper.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.BookKeeper.models.Book;
+import ru.BookKeeper.models.Person;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+public class PersonDAO {
+
+
+
+}
